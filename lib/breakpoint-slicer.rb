ecea0ae9b4c2d0674cdd8a428619a94acd3455df
Andrey 'lolmaus' Mikhaylov <lolmaus@gmail.com>
2014-06-15 04:15:13 +0400

    Version bump

--- lib/breakpoint-slicer.rb
+++ lib/breakpoint-slicer.rb
@@ -4,6 +4,6 @@ require 'breakpoint'
 Compass::Frameworks.register("breakpoint-slicer", :path => "#{File.dirname(__FILE__)}/..")
 
 module BreakpointSlicer
-  VERSION = "1.3.3"
-  DATE = "2014-06-14"
+  VERSION = "1.3.4"
+  DATE = "2014-06-15"
 end
