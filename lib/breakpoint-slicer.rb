a097c6d0e01037835342caa3baf1d122d28d471f
lolmaus (Andrey Mikhaylov) <lolmaus@gmail.com>
2014-10-23 18:19:29 +0400

    Fixed next-slice function name

--- lib/breakpoint-slicer.rb
+++ lib/breakpoint-slicer.rb
@@ -4,6 +4,6 @@ require 'breakpoint'
 Compass::Frameworks.register("breakpoint-slicer", :path => "#{File.dirname(__FILE__)}/..")
 
 module BreakpointSlicer
-  VERSION = "1.3.5.alpha-1"
+  VERSION = "1.3.5.alpha-2"
   DATE = "2014-10-23"
 end
