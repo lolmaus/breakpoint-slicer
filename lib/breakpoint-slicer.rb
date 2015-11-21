7aa02b0e1035c6c901ba931293af09b59219105c
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2015-11-21 18:12:29 +0300

    Updated version number in Ruby

--- lib/breakpoint-slicer.rb
+++ lib/breakpoint-slicer.rb
@@ -4,6 +4,6 @@ require 'breakpoint'
 Compass::Frameworks.register("breakpoint-slicer", :path => "#{File.dirname(__FILE__)}/..")
 
 module BreakpointSlicer
-  VERSION = "1.3.6"
-  DATE = "2015-01-05"
+  VERSION = "1.4.0"
+  DATE = "2015-11-21"
 end
